ae935a38085d722efacfefbe3d6f848f938348cd
TLNRB <nortol01@easv365.dk>
2023-02-22 09:00:25 +0100

    Local repo

---
+++ wp-default-template/footer.php
@@ -0,0 +1,10 @@
+<?php
+/** 
+ * This is a template that display all the <footer> elements.
+ * Contains all the HTML closing tags.
+*/
+?>
+
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-default-template/header.php
@@ -0,0 +1,17 @@
+<?php
+/** 
+ * This is a template that display all the <head> elements until the <main>.
+*/
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello</title>
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
